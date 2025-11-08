c0307381bdfd7916e2c989c9456a6d63d6029862
Kevin Soto <sotoorta93@gmail.com>
2025-11-07 22:42:55 -0300

    added webserver

--- playwright.config.ts
+++ playwright.config.ts
@@ -69,9 +69,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run dev',          // cómo levantar tu app
+    url: 'http://localhost:5173',    // debe coincidir con baseURL
+    reuseExistingServer: !process.env.CI,
+    timeout: 60 * 1000,              // (opcional) 1 minuto para que arranque
+  },
 });
